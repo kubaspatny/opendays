98899317b9d2675fccae48c36272e189b4e217cb
spatnjak <spatnjak@fel.cvut.cz>
2015-03-15 11:27:12 +0100

    DataContract - added other tables

--- app/src/main/java/cz/kubaspatny/opendays/database/DataContract.java
+++ app/src/main/java/cz/kubaspatny/opendays/database/DataContract.java
@@ -28,6 +28,72 @@ public class DataContract {
         public static final String COLUMN_NAME_ROUTE_TIMESTAMP = "route_timestamp";
         public static final String COLUMN_NAME_EVENT_ID = "eventid";
         public static final String COLUMN_NAME_EVENT_NAME = "event_name";
+        // TODO: remove unused columns, to get rid of data duplication
+
+    }
+
+    public static abstract class Route implements BaseColumns {
+
+        public static final String TABLE_NAME = "route";
+        public static final Uri CONTENT_URI = DbContentProvider.CONTENT_URI.buildUpon().path(TABLE_NAME).build();
+
+        public static final String COLUMN_NAME_ROUTE_ID = "routeid";
+        public static final String COLUMN_NAME_ROUTE_NAME = "route_name";
+        public static final String COLUMN_NAME_ROUTE_COLOR = "route_color";
+        public static final String COLUMN_NAME_ROUTE_INFORMATION = "route_information";
+        public static final String COLUMN_NAME_ROUTE_TIMESTAMP = "route_timestamp";
+
+        public static final String COLUMN_NAME_EVENT_ID = "eventid";
+        public static final String COLUMN_NAME_EVENT_NAME = "event_name";
+        public static final String COLUMN_NAME_EVENT_INFORMATION = "event_information";
+
+    }
+
+    public static abstract class Station implements BaseColumns {
+
+        public static final String TABLE_NAME = "station";
+        public static final Uri CONTENT_URI = DbContentProvider.CONTENT_URI.buildUpon().path(TABLE_NAME).build();
+
+        public static final String COLUMN_NAME_ROUTE_ID = "routeid";
+
+        public static final String COLUMN_NAME_STATION_ID = "stationid";
+        public static final String COLUMN_NAME_STATION_NAME = "station_name";
+        public static final String COLUMN_NAME_STATION_LOCATION = "station_location";
+        public static final String COLUMN_NAME_STATION_SEQ_POSITION = "station_seq_position";
+        public static final String COLUMN_NAME_STATION_TIME_LIMIT = "station_time_limit";
+        public static final String COLUMN_NAME_STATION_TIME_RELOCATION = "station_time_relocation";
+        public static final String COLUMN_NAME_STATION_STATUS = "station_status"; // OPENED, CLOSED
+
+    }
+
+    /**
+     * This table represents other groups locations. Current user's
+     * location update queue is in table LocationUpdates.
+     */
+    public static abstract class GroupLocations implements BaseColumns {
+
+        public static final String TABLE_NAME = "grouplocations";
+        public static final Uri CONTENT_URI = DbContentProvider.CONTENT_URI.buildUpon().path(TABLE_NAME).build();
+
+        public static final String COLUMN_NAME_ROUTE_ID = "routeid";
+        public static final String COLUMN_NAME_STATION_ID = "stationid";
+        public static final String COLUMN_NAME_LOCATION_UPDATE_TYPE = "location_update_type"; // CHECKIN, CHECKOUT, SKIP
+        public static final String COLUMN_NAME_GROUP_ID = "groupid";
+        public static final String COLUMN_NAME_GROUP_GUIDE = "group_guide";
+        public static final String COLUMN_NAME_GROUP_STATUS = "group_status";
+        public static final String COLUMN_NAME_GROUP_SEQ_POSITION = "group_status";
+
+    }
+
+    public static abstract class LocationUpdates implements BaseColumns {
+
+        public static final String TABLE_NAME = "locationupdates";
+        public static final Uri CONTENT_URI = DbContentProvider.CONTENT_URI.buildUpon().path(TABLE_NAME).build();
+
+        public static final String COLUMN_NAME_TIMESTAMP = "timestamp";
+        public static final String COLUMN_NAME_STATION_ID = "stationid";
+        public static final String COLUMN_NAME_LOCATION_UPDATE_TYPE = "location_update_type"; // CHECKIN, CHECKOUT, SKIP
+        public static final String COLUMN_NAME_GROUP_ID = "groupid";
 
     }
 
